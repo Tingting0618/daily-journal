c93ec5b9e64a3b30a3c2ff66cf86775026d320ac
Tingting Duan <duan.tingting@yahoo.com>
2021-05-01 16:04:34 -0500

    Book2_DailyNote_Completed

--- scripts/database.js
+++ scripts/database.js
@@ -38,7 +38,51 @@ database.entries.push(noteAboutToday)
 console.log(database)
 
 
+for (const noteObj of database.entries) {
+    console.log(
+`Note ${noteObj.id}
+${noteObj.date}
+I learned ${noteObj.concept}
+I felt  ${noteObj.sleepy}
+---------
+`)
+}
 
+const searchTerm = "sleepy"
+for (const noteObj of database.entries) {
+    if (noteObj.mood===searchTerm) {
+        console.log(
+            `Note ${noteObj.id}
+            ${noteObj.date}
+            I learned ${noteObj.concept}
+            I felt  ${noteObj.sleepy}
+            ---------
+            `)
+    }
+
+}
+
+const createNote  = (noteObject) => {
+
+        const lastIndex = database.entries.length - 1
+        const currentLastNote = database.entries[lastIndex]
+        const maxId = currentLastNote.id
+        const idForNewNote = maxId + 1
+        noteObject.id = idForNewNote
+
+        database.entries.push(noteObject)
+}
+
+// Create a new note object
+const moreNewerNote = {
+    id: 4,
+    date: "05/01/2021",
+    concept: "Book2-Daily-Note",
+    entry: "I learned more about JS",
+    mood: "happy"
+}
+
+createNote(moreNewerNote)
 
 /*
     You export a function that provides a version of the
